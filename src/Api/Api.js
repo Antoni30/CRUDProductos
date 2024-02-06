95665c87319e1d6851134cd8a19e967856067f67
Cristian Tello <cristian.andres.t98@gmail.com>
2024-02-06 13:49:29 -0500

    Delete done

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,45 +1,53 @@
-import { addDoc, collection, deleteDoc, doc, getDocs} from 'firebase/firestore/lite';
-import { database } from './firebaseApi';
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  updateDoc,
+} from "firebase/firestore/lite";
+import { database } from "./firebaseApi";
 
 export const postProducts = async (product) => {
-    try {
-        const getCollection = await collection(database,'products');
-        await addDoc(getCollection,product)
-        console.log('Producto agregado correctamente');
-    } catch (error) {
-        console.error('Error al agregar el producto:', error);
-    }
+  try {
+    const getCollection = await collection(database, "products");
+    await addDoc(getCollection, product);
+    console.log("Producto agregado correctamente");
+  } catch (error) {
+    console.error("Error al agregar el producto:", error);
+  }
 };
-export const getProducs = async ()=>{
-    try{
-        const getCollection = await collection(database, 'products');
-        const docsE = await getDocs(getCollection);
-        
-        const listProducts = docsE.docs.map(doc => ({
-          id: doc.id,
-          data: doc.data(),
-        }));
-        
-        return listProducts;
-    }catch(error){
-        console.log(error)
-    }
-}
-export const updateProduct = async(productId,newData)=>{
-    try{
-        const productRef = await doc(database, 'products', productId);
-        await updateDoc(productRef, newData);
-        console.log('Producto actualizado correctamente');
-    }catch(error){
-        console.log(error)
-    }
-}
-export const deleteProduct = async(productId)=>{
-    try{
-        const productRef = doc(database, 'products', productId);
-        await deleteDoc(productRef)
-        console.log('Producto eliminado  correctamente');
-    }catch(error){
-        console.log(error)
-    }
-}
+export const getProducs = async () => {
+  try {
+    const getCollection = await collection(database, "products");
+    const docsE = await getDocs(getCollection);
+
+    const listProducts = docsE.docs.map((doc) => ({
+      id: doc.id,
+      data: doc.data(),
+    }));
+
+    return listProducts;
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const updateProduct = async (productId, newData) => {
+  try {
+    const productRef = await doc(database, "products", productId);
+    console.log(newData);
+    await updateDoc(productRef, newData);
+    console.log("Producto actualizado correctamente");
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const deleteProduct = async (productId) => {
+  try {
+    const productRef = doc(database, "products", productId);
+    await deleteDoc(productRef);
+    console.log("Producto eliminado  correctamente");
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -2,16 +2,16 @@ import ProductList from "../components/ProductList";
 import ProductAdd from "../components/ProductAdd";
 
 const ProductPage = () => {
-    return (
-        <div className="grid grid-cols-1 sm:max-xl:grid-cols-2 gap-4 auto-rows-auto">
-          <div className="flex flex-col items-center justify-center mt-8 md:max-xl:ml-7">
-            <ProductList />   
-          </div>
-          <div>
-            <ProductAdd />
-          </div>
-        </div>
-      );
+  return (
+    <div className="grid grid-cols-1 sm:max-xl:grid-cols-2 gap-4 auto-rows-auto">
+      <div className="flex flex-col items-center justify-center mt-8 md:max-xl:ml-7">
+        <ProductList />
+      </div>
+      <div>
+        <ProductAdd />
+      </div>
+    </div>
+  );
 };
 
 export default ProductPage;
